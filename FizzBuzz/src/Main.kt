19939919d7ad8208aa01ee7be2c14fae6cc7495a
Yoshitomo Minami <minami530@gmail.com>
2017-12-08 21:28:36 +0900

    Added spring framework demo

--- FizzBuzz/src/Main.kt
+++ FizzBuzz/src/Main.kt
@@ -3,10 +3,10 @@ package fizzbuzz
 fun main(args: Array<String>) {
     for (i in 1..100) {
         val num = when {
-            i % 5 == 0 -> "FizzBuzz"
-            i % 3 == 0 -> "Fizz"
-            i % 5 == 0 -> "Buzz"
-            else       -> "$i"
+            i % 15 == 0 -> "FizzBuzz"
+            i % 3 == 0  -> "Fizz"
+            i % 5 == 0  -> "Buzz"
+            else        -> "$i"
         }
         println(num)
     }
---
+++ spring-todo/src/main/kotlin/com/example/todolist/TaskController.kt
@@ -0,0 +1,64 @@
+package com.example.todolist
+
+
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.stereotype.Controller
+import org.springframework.validation.BindingResult
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PathVariable
+
+
+@Controller
+@RequestMapping("tasks")
+class TaskController(private val taskRepository: TaskRepository) {
+
+    @GetMapping("")
+    fun index(model: Model): String {
+        val tasks = taskRepository.findAll()
+        model.addAttribute("tasks", tasks)
+        return "tasks/index"
+    }
+
+    @GetMapping("new")
+    fun new(form: TaskCreateForm): String {
+        return "tasks/new"
+    }
+
+    @PostMapping("")
+    fun create(@Validated form: TaskCreateForm,
+               bindingResult: BindingResult): String {
+        if (bindingResult.hasErrors())
+            return "tasks/new"
+
+        val content = requireNotNull(form.content)
+        taskRepository.create(content)
+        return "redirect:/tasks"
+    }
+
+    @GetMapping("{id}/edit")
+    fun edit(@PathVariable("id") id: Long,
+             form: TaskUpdateForm): String {
+        val task = taskRepository.findById(id) ?: throw NotFoundException()
+        form.content = task.content
+        form.done = task.done
+        return "tasks/edit"
+    }
+
+    @PatchMapping("{id}")
+    fun update(@PathVariable("id") id: Long,
+               @Validated form: TaskUpdateForm,
+               bidingResult: BindingResult): String {
+        if (bidingResult.hasErrors())
+            return "tasks/edit"
+
+        val task = taskRepository.findById(id) ?: throw NotFoundException()
+        val newTask = task.copy(content = requireNotNull(form.content),
+                done = form.done)
+        taskRepository.update(newTask)
+        return "redirect:/tasks"
+    }
+}
