e262280352f83ab2915b1e24eaecce55257b9bbe
monmaru <minami530@gmail.com>
2021-04-12 00:57:35 +0900

    Overlay a logo on a QR code

--- qr-code-service/src/main/kotlin/App.kt
+++ qr-code-service/src/main/kotlin/App.kt
@@ -1,5 +1,6 @@
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.EncodeHintType
+import com.google.zxing.client.j2se.MatrixToImageConfig
 import com.google.zxing.client.j2se.MatrixToImageWriter
 import com.google.zxing.qrcode.QRCodeWriter
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
@@ -14,7 +15,13 @@ import io.ktor.server.cio.*
 import io.ktor.server.engine.*
 import io.ktor.util.*
 import kotlinx.serialization.Serializable
+import java.awt.AlphaComposite
+import java.awt.Graphics2D
+import java.awt.image.BufferedImage
 import java.io.ByteArrayOutputStream
+import java.io.File
+import javax.imageio.ImageIO
+import kotlin.math.roundToInt
 
 
 fun Application.module() {
@@ -50,9 +57,31 @@ fun main() {
 private fun generateQRCode(p: QRCodeParameter): ByteArray {
     val codeWriter = QRCodeWriter()
     val hints = mapOf(EncodeHintType.ERROR_CORRECTION to ErrorCorrectionLevel.H)
-    val bitMatrix = codeWriter.encode(p.text, BarcodeFormat.QR_CODE, p.width, p.height, hints)
+    val bitMatrix = codeWriter.encode(p.text, BarcodeFormat.QR_CODE, p.size, p.size, hints)
+    val config = MatrixToImageConfig(MatrixToImageConfig.BLACK, MatrixToImageConfig.WHITE)
+    val qrImage = MatrixToImageWriter.toBufferedImage(bitMatrix, config)
+    return if (p.useLogo) {
+        convertPngByteArray(overlayLogo(qrImage))
+    } else {
+        convertPngByteArray(qrImage)
+    }
+ }
+
+private fun overlayLogo(image: BufferedImage): BufferedImage {
+    val logoImage = ImageIO.read(File("./test.png"))
+    val deltaHeight = image.height - logoImage.height
+    val deltaWidth = image.width - logoImage.width
+    val combined = BufferedImage(image.height, image.width, BufferedImage.TYPE_INT_ARGB)
+    val g = combined.graphics as Graphics2D
+    g.drawImage(image, 0, 0, null)
+    g.composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f)
+    g.drawImage(logoImage, (deltaWidth / 2).toFloat().roundToInt(), (deltaHeight / 2).toFloat().roundToInt(), null)
+    return combined
+}
+
+private fun convertPngByteArray(image: BufferedImage): ByteArray {
     ByteArrayOutputStream().use {
-        MatrixToImageWriter.writeToStream(bitMatrix, "png", it)
+        ImageIO.write(image, "png", it)
         return it.toByteArray()
     }
 }
@@ -60,6 +89,6 @@ private fun generateQRCode(p: QRCodeParameter): ByteArray {
 @Serializable
 data class QRCodeParameter(
     val text: String,
-    val width: Int,
-    val height: Int
+    val size: Int,
+    val useLogo: Boolean
 )
